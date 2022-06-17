4f62b9081e7f1946e435baf38569847eab4a8091
surajguptatarento <suraj.gupta@tarento.com>
2022-06-17 16:37:23 +0530

    Cleanup

--- src/helpers/Contexts.js
+++ src/helpers/Contexts.js
@@ -1,6 +1,4 @@
 
 import { createContext } from "react";
 const GameStateContext = createContext("")
-export const dataContext= createContext('');
-export const dataContext1= createContext('')
 export default GameStateContext;
